fdd0995daee51b7f3f51241c47aeedde4c9955b5
Sagar327 <sagargahlyan738@gmail.com>
2026-07-24 22:34:19 +0530

    Improve YouTube download handling and update dependencies

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -10,18 +10,26 @@ os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 def download_youtube_audio(url:str) ->str:
     output_path=os.path.join(DOWNLOAD_DIR,"%(title)s.%(ext)s")
-    ydl_opts={
-        "format":"bestaudio/best",
-        "outtmpl":output_path,
-        "postprocessors":[
-            {
-                "key":"FFmpegExtractAudio",
-                "preferredcodec":"wav",
-                "preferredquality":"192",
-            }
-        ],
-        "quiet":True,  ## can remove this if we want to see the downloading log bar of the video but preferrable for testing phase
-    }
+    ydl_opts = {
+    "format": "bestaudio/best",
+    "outtmpl": output_path,
+    "quiet": False,
+    "noplaylist": True,
+    "extractor_args": {
+        "youtube": {
+            "player_client": ["android"]
+        }
+    },
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "wav",
+            "preferredquality": "192",
+        }
+    ],
+}
+    
+      
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info=ydl.extract_info(url,download=True)
         filename=ydl.prepare_filename(info).replace(".webm",".wav").replace(".m4a",".wav")
